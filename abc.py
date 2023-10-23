bb30001f57ed865d7069788988438a534bd206c9
Binal Shah <binalshah0210@gmail.com>
2023-10-23 15:42:48 -0500

    changes

--- abc.py
+++ abc.py
@@ -1,9 +1,5 @@
 make changes to this file
 import tensorflow as tf
-<<<<<<< Updated upstream
 import numpy as np
 changes made on github
-=======
-new changes
 lets check if restored or not
->>>>>>> Stashed changes
